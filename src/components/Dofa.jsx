dd07958bc661c98f0af5031855e45e7983a69758
Katy <jdiazk@uninorte.edu.co>
2024-09-16 15:59:40 -0500

    Add the info to the swot file

--- src/components/Dofa.jsx
+++ src/components/Dofa.jsx
@@ -1,9 +1,37 @@
+import { cardsInfo } from "../utils/info";
+function Card(tittle, items){
+    return(
+        <div className="block max-w p-6 bg-[#ffe7c9] border border-gray-200 rounded-2xl shadow hover:shadow-slate-800 h-auto">
+            <h2 className="mb-2 text-2xl font-bold traking-tight text-justify txt-black py-2">{tittle}</h2>
+            <ul>
+                {items.map((item)=> (
+                    <li key={item.id}>
+                        <h5 className="font-bold">{item.id}. {item.title}</h5>
+                        <p className="px-8 pb-1">{item.description}</p>
+                    </li>
+                )
+                )
+                }
+            </ul>
+        </div>
+    );
+}
+
 function Dofa() {
     return(
-        <section className="flex-col w-full h-auto bg-[#e2f0fb] px-6">
+        <section className="flex-col w-full h-auto bg-[#e2f0fb] px-6 py-2">
             <h1 className="font-bold text-2xl justify-items-start pl-2">
-                Current diagnosis 
+                Current diagnosis(SWOT)
             </h1>
+            <div className="grid grid-cols-2 grid-rows-2 gap-5 py-2">
+                {cardsInfo.map((card)=>{
+                    return(
+                        Card(card.type, card.items)
+                    )
+                }
+                )
+                }
+            </div>
         </section>
     );
 }
---
+++ src/utils/info.js
@@ -0,0 +1,87 @@
+export const cardsInfo = [
+    {   "id":1,
+        "type": "Strengths",
+        "items":[
+            {   "id":1, 
+                "title": "Product Variety",
+                "description": "The store offers a wide range of Funko Pops, from common editions to hard-to-find exclusives, appealing to both casual and serious collectors."
+            },
+            {
+                "id":2, 
+                "title": "Customer Loyalty",
+                "description": "Our community of loyal customers not only makes repeat purchases but also actively engages in events and promotions, strengthening the brand." 
+            },
+            {   "id":3, 
+                "title": "Excellent Customer Service:" ,
+                "description": "The store is known for its quick response to inquiries and its efficient return policy, which fosters trust and satisfaction among customers."
+            },
+            {   "id":4, 
+                "title": "Strong Social Media Presence:",
+                "description": "We have a solid social media strategy that connects the store with pop culture fans, creating a strong sense of community and increasing the visibility of our products."
+            },
+        ]
+    },
+    {   "id":2,
+        "type": "Weakness",
+        "items":[
+            {   "id":1, 
+                "title": "Reliance on Limited Suppliers:",
+                "description": "The store depends on a limited number of suppliers, which can lead to inventory issues, especially for exclusive or limited-edition products."
+            },
+            {   "id":2, 
+                "title": "Logistics and Shipping:",
+                "description": "While the store offers international shipping, delivery times can be lengthy, which impacts customer satisfaction, particularly for buyers outside the country."
+            },
+            {   "id":3, 
+                "title": "Reduced Margins:",
+                "description": "The high competition in the Funko Pop market forces the store to maintain competitive pricing, reducing profit margins."
+            },
+            {   "id":4, 
+                "title": "Lack of Product Diversification:",
+                "description": "Currently, the store focuses exclusively on Funko Pops, limiting its appeal to collectors seeking other types of pop culture-related products."
+            }
+        ]
+    },
+    {   "id":3,
+        "type": "Opportunities",
+        "items":[
+            {   "id":1, 
+                "title": "Expansion into New Markets:",
+                "description": "The growing popularity of Funko Pops in international markets, especially in Asia and Latin America, provides an excellent opportunity to expand the store globally."
+            },
+            {   "id":2, 
+                "title": "Collaborations with Influencers and YouTubers:",
+                "description": "Partnering with influencers and content creators in the pop culture space can increase the store’s visibility and attract new customers."
+            },
+            {   "id":3, 
+                "title": "Launch of New Products",
+                "description": "The store could expand its offering to include other collectibles (action figures, themed clothing, accessories), increasing the average ticket value of each purchase."
+            },
+            {   "id":4, 
+                "title": "Growing Geek Culture Trend:",
+                "description": "The rising popularity of film, series, and video game franchises offers a constant opportunity to release new products tied to Funko Pops, driven by major releases or cultural events."
+            }
+        ]
+    },
+    {   "id":4,
+        "type": "Threats",
+        "items":[
+            {   "id":1, 
+                "title": "Intense Competition:",
+                "description": "The Funko Pop market is highly competitive, with both large online retailers and small specialized sellers. Price competition and product availability can affect customer loyalty."
+            },
+            {   "id":2, 
+                "title": "Demand Fluctuations:",
+                "description": "The demand for Funko Pops can be highly unpredictable and fluctuates depending on pop culture trends, potentially causing overstock or shortages."
+            },
+            {   "id":3, 
+                "title": "Supply Chain Issues:",
+                "description": "Dependence on external suppliers and potential disruptions in the global supply chain could affect product availability and lead to shipping delays."
+            },
+            {   "id":4, 
+                "title": "Shifting Consumer Preferences:",
+                "description": "As new trends and collectibles emerge, consumers may lose interest in Funko Pops and seek other types of products, leading to a decline in demand."
+            }
+        ]
+    },
+]
